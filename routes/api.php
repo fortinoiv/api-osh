8d19a2b5080b4fec1e6a06e18d566d002c1ca22e
Fortino Inocente Vicente <fortino.inocente@gmail.com>
2022-07-27 16:29:58 -0600

    Se agrega JWT

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\V1\ProductsController;
+use App\Http\Controllers\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::prefix('v1')->group(function () {
+    //Prefijo V1, todo lo que este dentro de este grupo se accedera escribiendo v1 en el navegador, es decir /api/v1/*
+    Route::post('login', [AuthController::class, 'authenticate']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::get('products', [ProductsController::class, 'index']);
+    Route::get('products/{id}', [ProductsController::class, 'show']);
+    Route::group(['middleware' => ['jwt.verify']], function() {
+        //Todo lo que este dentro de este grupo requiere verificación de usuario.
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('get-user', [AuthController::class, 'getUser']);
+        Route::post('products', [ProductsController::class, 'store']);
+        Route::put('products/{id}', [ProductsController::class, 'update']);
+        Route::delete('products/{id}', [ProductsController::class, 'destroy']);
+    });
 });
